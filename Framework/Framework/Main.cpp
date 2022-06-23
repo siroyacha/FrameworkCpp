c3ccb00523dcee10c7e7981f69467ed90d4964fa
jae_hyeok <wogur2202@gmail.com>
2022-06-23 18:25:07 +0900

    Framework v.0.2

--- Framework/Framework/Main.cpp
+++ Framework/Framework/Main.cpp
@@ -1,1 +1,36 @@
 // ** Framework v.0.2
+#include <iostream>
+
+namespace AAA
+{
+	void Output()
+	{
+		printf_s("AAA");
+	}
+}
+
+namespace BBB
+{
+	void Output()
+	{
+		printf_s("BBB");
+	}
+}
+
+namespace CCC
+{
+	void Output()
+	{
+		printf_s("CCC");
+	}
+}
+
+int main(void)
+{
+	AAA::Output();
+
+
+
+
+	return 0;
+}
